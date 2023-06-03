3bdced131d258a24397f672cee6d98edd1c89b71
TajulDev <mdtajul.freelancer@gmail.com>
2023-06-03 13:30:57 +0600

    form

--- src/Components/Home/Form/Form.js
+++ src/Components/Home/Form/Form.js
@@ -4,7 +4,7 @@ import { useFormik } from "formik";
 
 const Form = () => {
 
-    const [data, setData]= useState("")
+  const [data, setData]= useState([])
   const formik = useFormik({
     initialValues: {
       name: "",
@@ -18,36 +18,40 @@ const Form = () => {
     onSubmit: (values, { resetForm }) => {
       console.log(values);
       resetForm({ values: "" });
-      localStorage.setItem("doner", JSON.stringify(values));
-      setData(values)
-    },
+      localStorage.setItem("doner", JSON.stringify([...data,values]));                  
+      setData(current=>[...current,values])
+    },  
+    
   });
 
   const handleDelete =()=>{
     localStorage.removeItem("doner")
     setData("")
-  }
+  } 
 
   useEffect(()=>{
     const newData = localStorage.getItem("doner")
-    setData(JSON.parse(newData))
+    if(newData){
+      setData(JSON.parse(newData))
+    } 
+
   },[])
 
-
+  console.log(data,"dddd")
 
   return (
     <div className="info__form max-w-[555px]">
       <div>
-        <h3 className="form__title text-[28px] leading-[36px] text-[#fff] p-[30px] bg-[#4E4E4E] font-[700] text-center">
-          {" "}
-          REQUEST APPOINTMENT{" "}
+        <h3 className="form__title text-[28px] leading-[36px] text-[#fff] p-[30px] bg-[#4E4E4E] font-[700] text-center"> 
+          {/* {" "} */}
+          REQUEST APPOINTMENT
         </h3>
       </div>
       <div className="px-[35px] pt-[40px] pb-[20px] bg-[#eaedf1]">
         <form
           onSubmit={formik.handleSubmit}
           className="grid -grid-cols-1 gap-[24px]"
-        >
+        >    
           <div className="grid grid-cols-2 gap-[35px]">
             <input
               onChange={formik.handleChange}
@@ -107,7 +111,7 @@ const Form = () => {
               type="text"
               name="time"
               id="time"
-              value={formik.values.time}
+              value={formik.values.time} 
             />
           </div>
           <div>
@@ -126,14 +130,25 @@ const Form = () => {
           </button>
         </form>
 
-        <button onClick={handleDelete}>delete</button>
+        {/* <button onClick={handleDelete}>delete</button>
         <p>{data?.name}</p>
         <p>{data?.email}</p>
         <p>{data?.phone}</p>
         <p>{data?.center}</p>
         <p>{data?.date}</p>
         <p>{data?.time}</p>
-        <p>{data?.comment}</p>
+        <p>{data?.comment}</p> */}
+
+        {data.map(item=><div className="bg-red-400 p-5 m-5">
+          <p>{item?.name}</p>
+        <p>{item?.email}</p>
+        <p>{item?.phone}</p>
+        <p>{item?.center}</p>
+        <p>{item?.date}</p>
+        <p>{item?.time}</p>
+        <p>{item?.comment}</p> 
+        {/* <button onClick={handleDelete}>delete</button> */}
+        </div>)}
       </div>
     </div>
   );
